cddecdbe19a8c3352bdc719b7048e1093a9e33f1
Jeka shiyan <ncringe31@gmail.com>
2021-02-17 10:40:34 +0200

    change package json and add swiper vanila lazyload bootstrap and jquery

---
+++ src/blocks/modules/header/burger.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger");
+
+const burgerOpen = () => {
+    burger.classList.toggle("burger-close");
+};
+
+burger.addEventListener("click", burgerOpen);
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,4 @@
 import "%modules%/header/header";
+import "%modules%/header/burger";
+import "%modules%/hero/hero";
 import "%modules%/footer/footer";
