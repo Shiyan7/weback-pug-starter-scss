e5c30535bd37c210de0a2bfd834318904cc82f80
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-06-27 00:46:21 +0300

    some fixed

--- src/blocks/modules/header/burger.js
+++ src/blocks/modules/header/burger.js
@@ -1,7 +1,9 @@
-const burger = document.querySelector(".burger");
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
 
-const burgerOpen = () => {
-    burger.classList.toggle("burger-close");
-};
-
-burger.addEventListener("click", burgerOpen);
+    const burgerOpen = () => {
+        burger.classList.toggle("burger-close");
+    };
+    
+    burger?.addEventListener("click", burgerOpen);
+});
